0338ae640ad7757812afcd2cfa02230272326571
kojesung <js95112345@gmail.com>
2024-12-30 21:36:22 +0900

    data fetching in client side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,20 @@
-export const metadata = {
-    title: 'Home',
-};
+'use client';
+
+import { useEffect, useState } from 'react';
 
 export default function Page() {
-    return <h1>hello next</h1>;
+    const URL = 'https://nomad-movies.nomadcoders.workers.dev';
+    const [isLoading, setIsLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const response = await fetch(URL + '/movies');
+        const json = await response.json(); // response.json()은 Promise반환
+        setMovies(json);
+        setIsLoading(false);
+    };
+    useEffect(() => {
+        getMovies();
+    }, []);
+
+    return <div>{isLoading ? <div>Loading...</div> : JSON.stringify(movies)}</div>;
 }
