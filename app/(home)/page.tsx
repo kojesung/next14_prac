daadf7c7f99f9007ec31c52a34f7c9255af3cf52
kojesung <js95112345@gmail.com>
2024-12-31 21:01:15 +0900

    2개의 data fetching 순차적으로 요청

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,18 +1,28 @@
+import Link from 'next/link';
+
 export const metadata = {
     title: 'Home',
 };
 
-const URL = 'https://nomad-movies.nomadcoders.workers.dev'; // 변수가 꼭 컴포넌트 내부에 있지 않아도 됨
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev'; // 다른 파일에서 사용할 수 있도록 export
 
 const getMovie = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 5000)); // 로딩시간 주기 위해 5초 지연
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // 로딩시간 주기 위해 5초 지연
     // 하나의 컴포넌트가 로드가 완료되지 않으면 Loading.tsx 파일 사용(data fetching중이라고 판단)
-    const response = await fetch(URL + '/movies');
+    const response = await fetch(API_URL + '/movies');
     const json = await response.json();
     return json;
 };
 
 export default async function Page() {
     const movies = await getMovie();
-    return <div>{JSON.stringify(movies)}</div>;
+    return (
+        <div>
+            {movies.map((movie) => (
+                <li key={movie.id}>
+                    <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+                </li>
+            ))}
+        </div>
+    );
 }
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,12 +1,23 @@
-export default async function MovieDetails({
-    params,
-    searchParams,
-}: {
-    params: { id: string };
-    searchParams: { any };
-}) {
+import { API_URL } from '../../../(home)/page';
+
+export default async function MovieDetails({ params }: { params: { id: string } }) {
+    const getMovie = async (id: string) => {
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // 로딩시간 주기 위해 5초 지연
+        const response = await fetch(`${API_URL}/movies/${urlParams.id}`);
+        return response.json();
+    };
+
+    const getVideo = async (id: string) => {
+        const response = await fetch(`${API_URL}/movies/${urlParams.id}/videos`);
+        return response.json();
+    };
     const urlParams = await params;
-    const urlSearchParams = await searchParams;
-    console.log(urlParams, urlSearchParams);
-    return <h1>Movie {urlParams.id}</h1>;
+    const movie = await getMovie(urlParams.id);
+    const video = await getVideo(urlParams.id);
+    return (
+        <div>
+            <h1>{movie.title}</h1>
+            <h2>{JSON.stringify(video)}</h2>
+        </div>
+    );
 }
