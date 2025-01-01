397bdf15b157b677866972fa4bffd30e5d83c1f4
kojesung <js95112345@gmail.com>
2025-01-01 17:54:57 +0900

    에러 빼기

--- components/movie-video.tsx
+++ components/movie-video.tsx
@@ -3,7 +3,6 @@ import { API_URL } from '../app/(home)/page';
 const getVideo = async (id: string) => {
     console.log(Date.now());
     await new Promise((resolve) => setTimeout(resolve, 5000)); // 로딩시간 주기 위해 5초 지연
-    throw new Error('고장남!');
     const response = await fetch(`${API_URL}/movies/${id}/videos`);
     return response.json();
 };
