8bd1f49c88c8fda0053b078aeefdab4e58bc2b1a
kojesung <js95112345@gmail.com>
2025-01-04 16:15:19 +0900

    영화 정보 css

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { decodeFormState } from 'next/dist/server/app-render/entry-base';
 import { API_URL } from '../../../(home)/page';
 import { Suspense } from 'react';
-import MovieInfo from '../../../../components/movie-info';
+import MovieInfo from '../../../../components/movie-info/movie-info';
 import MovieVideo from '../../../../components/movie-video';
 
 export default async function MovieDetails({ params }: { params: { id: string } }) {
---
+++ components/movie-info/movie-info.tsx
@@ -0,0 +1,22 @@
+import { API_URL } from '../../app/(home)/page';
+import styles from './movie-infor.module.css';
+
+const getMovie = async (id: string) => {
+    console.log(Date.now());
+    const response = await fetch(`${API_URL}/movies/${id}`);
+    return response.json();
+};
+
+export default async function MovieInfo({ id }: { id: string }) {
+    const movieInfo = await getMovie(id);
+    return (
+        <div className={styles.container}>
+            <img className={styles.poster} src={movieInfo.poster_path} alt={movieInfo.title} />
+            <div>
+                <h1 className={styles.title}>{movieInfo.title}</h1>
+                <h3 className={styles.info}>⭐️{movieInfo.vote_average.toFixed(1)}</h3>
+                <p className={styles.info}>{movieInfo.overview}</p>
+            </div>
+        </div>
+    );
+}
